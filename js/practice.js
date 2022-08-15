bc4a36ba9b8955e8876a53eefe8519ed629f2431
Ajitesh Newnaha <ajitesh.newnaha@phasezero.ai>
2022-08-15 18:34:37 +0530

    solve the given JavaScript assignment

---
+++ js/practice.js
@@ -0,0 +1,15 @@
+// create a variable
+
+// print the variable
+
+// assign value to variable
+
+// create a variable and assign string value
+
+// create a variable and assign number value
+
+// create a variable and assign boolean value
+
+// create a variable and assign array value
+
+// create a variable and assign object value
