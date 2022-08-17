28219622052d58673f8d73e837a57c1ad95b25f3
M, Abhishek <abhishekm.1582@outlook.com>
2022-08-17 15:46:31 +0530

    loops excercise

--- src/js/string.js
+++ src/js/string.js
@@ -1,25 +1,28 @@
 console.log('string.js loaded');
 
 const stringConcat = function () {
-    const solution = null;
+    let solution = null;
     // write your code
-
-
+    let str1 = 'Hello', str2 = 'World';
+    solution = str1 + str2;
+    
+    console.log(solution);
     document.querySelector('#strConcat').innerHTML = solution;
 }
-
+stringConcat();
 const strLength = function () {
-    const solution = null;
+    let solution = null;
     // write your code
 
 
     document.querySelector('#strLength').innerHTML = solution;
 }
-
+strLength();
 const strCharAt = function () {
-    const solution = null;
+    let solution = null;
     // write your code
 
 
     document.querySelector('#strCharAt').innerHTML = solution;
 }
+strCharAt();
---
+++ src/practice/js/loops.js
@@ -0,0 +1,49 @@
+let example;
+
+example = 'Type of transporation modes are: <br/>';
+example += '1. Air<br/>';
+example += '2. Land<br/>';
+example += '3. Water<br/>';
+
+document.getElementById('example').innerHTML = example;
+
+let while_Loop = 'Printing data using while loop: <br>';
+
+let forLoop = 'Printing data using for loop: <br>';
+
+let automobileCompanies = ['Alfa Romero', 'BMW', 'Cadillac', 'Dodge', 'Eicher', 'Ford', 'GMC', 'Hyundai', 'Infiniti', 'Jaguar', 'Koenigsegg', 'Lamborghini', 'Maserati', 'Nissan', 'Opel', 'Peugeot', 'Qoros', 'Rolls-Royce', 'Suzuki', 'Tesla', 'Ultima', 'Volkswagen', 'Wanderer', 'XPeng', 'Yulon', 'Zenos'];
+
+
+// write a while loop and display all the automobileCompanies
+let count = 0;
+while (count < automobileCompanies.length) {
+    
+    
+    count++;
+}
+
+
+
+
+
+
+
+
+
+
+document.getElementById('whileLoop').innerHTML = while_Loop;
+
+// write a for loop and display all the automobileCompanies
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('forLoop').innerHTML = forLoop;
